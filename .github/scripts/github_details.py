7b0783e723df3b427ee22bd18dd605b1e9110ce6
Korey Tibbs <korey.tibbs@Koreys-MacBook-Pro.local>
2023-12-07 20:51:42 -0800

    updating workflow python script

--- .github/scripts/github_details.py
+++ .github/scripts/github_details.py
@@ -15,17 +15,28 @@ def get_github_events(event_type):
         'Accept': 'application/vnd.github.v3+json'
     }
 
-    # Make the request
-    response = requests.get(api_url, headers=headers)
+    # Print debug information
+    print("API URL:", api_url)
+    print("Headers:", headers)
 
-    # Check for successful response
-    if response.status_code == 200:
-        events = response.json()
-        # Filter for specific event type (push or pull_request)
-        filtered_events = [event for event in events if event['type'] == event_type]
-        return filtered_events
-    else:
-        print(f"Error: Unable to fetch data (Status code: {response.status_code})")
+    try:
+        # Make the request
+        response = requests.get(api_url, headers=headers)
+
+        # Print raw response for debugging
+        print("Raw Response:", response.text)
+
+        # Check for successful response
+        if response.status_code == 200:
+            events = response.json()
+            # Filter for specific event type (push or pull_request)
+            filtered_events = [event for event in events if event['type'] == event_type]
+            return filtered_events
+        else:
+            print(f"Error: Unable to fetch data (Status code: {response.status_code})")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return None
 
 # Example usage
