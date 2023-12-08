cfb166a4d3f1fd5733f107822bf56c05a17e98de
Korey Tibbs <korey.tibbs@Koreys-MacBook-Pro.local>
2023-12-07 19:58:40 -0800

    adding workflows

--- .github/scripts/github_details.py
+++ .github/scripts/github_details.py
@@ -20,24 +20,23 @@ def get_commit_details(repo, commit_sha, token):
     response = requests.get(url, headers=headers)
     return response.json()
 
+def create_message(commit_data):
+    author_name = commit_data.get('author', {}).get('name', 'Unknown Author')
+    commit_message = commit_data.get('message', 'No commit message')
+    message = f"New commit by {author_name}: {commit_message}"
+    return message
+
+
 def main():
-    token = os.getenv('GITHUB_TOKEN')  # Replace with your GitHub token
-    repo = os.getenv('GITHUB_REPOSITORY')  # Repository name in the format 'owner/repo'
-    event = os.getenv('GITHUB_EVENT_NAME')
+    webhook_url = os.getenv('GOOGLE_CHAT_WEBHOOK')
+    commit_data = {} # Replace this with your actual logic to get commit data
 
-    if event == 'pull_request':
-        pull_number = os.getenv('GITHUB_REF')
-        details = get_pull_request_details(repo, pull_number, token)
-        # Process the pull request details here
+    print("Commit data:", commit_data)  # Debugging line
+    message = create_message(commit_data)
+    payload = {"text": message}
 
-    elif event == 'push':
-        commit_sha = os.getenv('GITHUB_SHA')
-        details = get_commit_details(repo, commit_sha, token)
-        # Process the commit details here
-
-    # Convert details to a string or format as needed
-    message = json.dumps(details, indent=4)
-    print(message)
+    response = requests.post(webhook_url, json=payload)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
