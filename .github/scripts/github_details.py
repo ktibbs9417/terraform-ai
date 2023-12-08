87694affa8df7cabda89733644338b32cf0a8b59
Korey Tibbs <korey.tibbs@Koreys-MacBook-Pro.local>
2023-12-07 20:14:47 -0800

    updating workflow python script

--- .github/scripts/github_details.py
+++ .github/scripts/github_details.py
@@ -1,42 +1,32 @@
-import os
 import requests
-import json
-
-def get_pull_request_details(repo, pull_number, token):
-    """
-    Fetch details of a specific pull request.
-    """
-    url = f"https://api.github.com/repos/{repo}/pulls/{pull_number}"
-    headers = {"Authorization": f"token {token}"}
+import os
+def fetch_push_events(repo, token):
+    url = f"https://api.github.com/repos/{repo}/events"
+    headers = {'Authorization': f'token {token}'}
     response = requests.get(url, headers=headers)
-    return response.json()
+    if response.status_code == 200:
+        events = response.json()
+        push_events = [event for event in events if event['type'] == 'PushEvent']
+        return push_events
+    else:
+        return None
 
-def get_commit_details(repo, commit_sha, token):
-    """
-    Fetch details of a specific commit.
-    """
-    url = f"https://api.github.com/repos/{repo}/commits/{commit_sha}"
-    headers = {"Authorization": f"token {token}"}
+# Example usage
+repo = os.getenv("GITHUB_REPOSITORY")  # Replace with your repository
+token = os.getenv("GITHUB_TOKEN")   # Replace with your GitHub token
+print(f"Repo: {repo}\n")
+push_events = fetch_push_events(repo, token)
+print(push_events)
+
+def fetch_pull_requests(repo, token):
+    url = f"https://api.github.com/repos/{repo}/pulls"
+    headers = {'Authorization': f'token {token}'}
     response = requests.get(url, headers=headers)
-    return response.json()
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
-def create_message(commit_data):
-    author_name = commit_data.get('author', {}).get('name', 'Unknown Author')
-    commit_message = commit_data.get('message', 'No commit message')
-    message = f"New commit by {author_name}: {commit_message}"
-    return message
-
-
-def main():
-    webhook_url = os.getenv('GOOGLE_CHAT_WEBHOOK')
-    commit_data = {} # Replace this with your actual logic to get commit data
-
-    print("Commit data:", commit_data)  # Debugging line
-    message = create_message(commit_data)
-    payload = {"text": message}
-
-    response = requests.post(webhook_url, json=payload)
-    print(response.text)
-
-if __name__ == "__main__":
-    main()
+# Example usage
+pull_requests = fetch_pull_requests(repo, token)
+print(pull_requests)
