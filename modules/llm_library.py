c6393c5aaf2da0bffc95d245f2462311ab399c4c
Korey Tibbs <korey.tibbs@Koreys-MacBook-Pro.local>
2023-12-14 09:43:36 -0800

    Removed local FAISS db to have it remain in memory

--- modules/llm_library.py
+++ modules/llm_library.py
@@ -4,13 +4,14 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import (
     ChatPromptTemplate,
-    MessagesPlaceholder,
+    AIMessagePromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+from langchain.prompts import MessagesPlaceholder
 from langchain.chat_models import ChatGooglePalm
+from langchain_google_genai import ChatGoogleGenerativeAI
 import os
-from langchain.embeddings.cohere import CohereEmbeddings
 from dotenv import load_dotenv
 from langchain.chat_models import AzureChatOpenAI
 from langchain.embeddings.cohere import CohereEmbeddings
@@ -23,8 +24,6 @@ class LLMLibrary:
     def __init__(self):
         BASEDIR = os.path.abspath(os.path.dirname("main.py"))
         load_dotenv(os.path.join(BASEDIR, '.env'))
-        choere_api_key = os.getenv("COHERE_API_KEY")
-        self.embedding_function = CohereEmbeddings(model="embed-multilingual-v2.0", cohere_api_key=choere_api_key)
         os.environ["OPENAI_API_TYPE"] = os.getenv("OPENAI_API_TYPE")
         os.environ["OPENAI_API_VERSION"] = os.getenv("OPENAI_API_VERSION")
         os.environ["OPENAI_API_BASE"] = os.getenv("OPENAI_API_BASE")
@@ -35,7 +34,7 @@ class LLMLibrary:
     def get_llm(self):
         print(f"Loading LLM {st.session_state.llm}")
         if st.session_state.llm == "vertex":
-            return ChatGooglePalm(temperature=0.0)
+            return ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.0)
         elif st.session_state.llm == "openai":
             return AzureChatOpenAI(deployment_name="gpt-4", model_name="gpt-4", temperature=0.0)
         
@@ -47,9 +46,8 @@ class LLMLibrary:
             embedding=self.embedding_function
         )
     def ask(self):
-
         llm = self.get_llm()
-        print(f"LLM: {llm}\n")  
+        print(f"LLM: {llm}\n")
         vectordb = vectorstore
 
         system_template = '''
@@ -60,10 +58,11 @@ class LLMLibrary:
 
         '''
         messages = [
-            SystemMessagePromptTemplate.from_template(system_template),
             HumanMessagePromptTemplate.from_template("Provide Terraform to the following question: {question}:"),
+            #AIMessagePromptTemplate.from_template(system_template),
         ]
         prompt = ChatPromptTemplate.from_messages(messages)
+
         retriever = vectordb.as_retriever(
                     search_type="similarity",
                     search_kwargs={
@@ -77,13 +76,13 @@ class LLMLibrary:
         print(f"Initiating chat conversation memory\n")
         #print(f"Conversation Memory: {memory}\n")
         conversation_chain= ConversationalRetrievalChain.from_llm(
-              llm,
-              retriever=retriever,
-              memory=memory,
-              rephrase_question=True,
-              combine_docs_chain_kwargs={'prompt': prompt},
-              return_source_documents=True,
-              verbose=True,
+            llm,
+            retriever=retriever,
+            memory=memory,
+            rephrase_question=True,
+            combine_docs_chain_kwargs={'prompt': prompt},
+            return_source_documents=True,
+            verbose=True,
         )
         #print(f"Conversation chain: {conversation_chain}\n")
         return conversation_chain
